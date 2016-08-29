57b9b27d17b583c306fff1d40506865d4699e426
Jeremy Fox <atljeremy@me.com>
2016-08-28 21:06:17 -0400

    Adding TV target

---
+++ ExtensionsTV/ExtensionsTV.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionsTV.h
+//  ExtensionsTV
+//
+//  Created by Jeremy Fox on 8/28/16.
+//  Copyright © 2016 Jeremy Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtensionsTV.
+FOUNDATION_EXPORT double ExtensionsTVVersionNumber;
+
+//! Project version string for ExtensionsTV.
+FOUNDATION_EXPORT const unsigned char ExtensionsTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionsTV/PublicHeader.h>
+
+
