3a5b688720bce64e71708e2d0680ba0db27d4e3a
Jeremy Fox <atljeremy@me.com>
2020-08-28 16:13:38 -0400

    Updates for swift 5

--- Sources/UIColor.swift
+++ Sources/UIColor.swift
@@ -33,200 +33,200 @@ import UIKit
 
 public extension UIColor {
     
-    class func hsb(_ h: CGFloat, _ s: CGFloat, _ b: CGFloat) -> UIColor {
-        return UIColor(hue: h, saturation: s, brightness: b, alpha: 1.0)
+    static func hsb(_ h: CGFloat, _ s: CGFloat, _ b: CGFloat) -> UIColor {
+        UIColor(hue: h, saturation: s, brightness: b, alpha: 1.0)
     }
     
-    class func flatBlackColor() -> UIColor {
-        return hsb(0, 0, 17)
+    static var flatBlackColor: UIColor {
+        hsb(0, 0, 17)
     }
     
-    class func flatBlueColor() -> UIColor {
-        return hsb(224, 50, 63)
+    static var flatBlueColor: UIColor {
+        hsb(224, 50, 63)
     }
     
-    class func flatBrownColor() -> UIColor {
-        return hsb(24, 45, 37)
+    static var flatBrownColor: UIColor {
+        hsb(24, 45, 37)
     }
     
-    class func flatCoffeeColor() -> UIColor {
-        return hsb(25, 31, 64)
+    static var flatCoffeeColor: UIColor {
+        hsb(25, 31, 64)
     }
     
-    class func flatForestGreenColor() -> UIColor {
-        return hsb(138, 45, 37)
+    static var flatForestGreenColor: UIColor {
+        hsb(138, 45, 37)
     }
     
-    class func flatGrayColor() -> UIColor {
-        return hsb(184, 10, 65)
+    static var flatGrayColor: UIColor {
+        hsb(184, 10, 65)
     }
     
-    class func flatGreenColor() -> UIColor {
-        return hsb(145, 77, 80)
+    static var flatGreenColor: UIColor {
+        hsb(145, 77, 80)
     }
     
-    class func flatLimeColor() -> UIColor {
-        return hsb(74, 70, 78)
+    static var flatLimeColor: UIColor {
+        hsb(74, 70, 78)
     }
     
-    class func flatMagentaColor() -> UIColor {
-        return hsb(283, 51, 71)
+    static var flatMagentaColor: UIColor {
+        hsb(283, 51, 71)
     }
     
-    class func flatMaroonColor() -> UIColor {
-        return hsb(5, 65, 47)
+    static var flatMaroonColor: UIColor {
+        hsb(5, 65, 47)
     }
     
-    class func flatMintColor() -> UIColor {
-        return hsb(168, 86, 74)
+    static var flatMintColor: UIColor {
+        hsb(168, 86, 74)
     }
     
-    class func flatNavyBlueColor() -> UIColor {
-        return hsb(210, 45, 37)
+    static var flatNavyBlueColor: UIColor {
+        hsb(210, 45, 37)
     }
     
-    class func flatOrangeColor() -> UIColor {
-        return hsb(28, 85, 90)
+    static var flatOrangeColor: UIColor {
+        hsb(28, 85, 90)
     }
     
-    class func flatPinkColor() -> UIColor {
-        return hsb(324, 49, 96)
+    static var flatPinkColor: UIColor {
+        hsb(324, 49, 96)
     }
     
-    class func flatPlumColor() -> UIColor {
-        return hsb(300, 45, 37)
+    static var flatPlumColor: UIColor {
+        hsb(300, 45, 37)
     }
     
-    class func flatPowderBlueColor() -> UIColor {
-        return hsb(222, 24, 95)
+    static var flatPowderBlueColor: UIColor {
+        hsb(222, 24, 95)
     }
     
-    class func flatPurpleColor() -> UIColor {
-        return hsb(253, 52, 77)
+    static var flatPurpleColor: UIColor {
+        hsb(253, 52, 77)
     }
     
-    class func flatRedColor() -> UIColor {
-        return hsb(6, 74, 91)
+    static var flatRedColor: UIColor {
+        hsb(6, 74, 91)
     }
     
-    class func flatSandColor() -> UIColor {
-        return hsb(42, 25, 94)
+    static var flatSandColor: UIColor {
+        hsb(42, 25, 94)
     }
     
-    class func flatSkyBlueColor() -> UIColor {
-        return hsb(204, 76, 86)
+    static var flatSkyBlueColor: UIColor {
+        hsb(204, 76, 86)
     }
     
-    class func flatTealColor() -> UIColor {
-        return hsb(195, 55, 51)
+    static var flatTealColor: UIColor {
+        hsb(195, 55, 51)
     }
     
-    class func flatWatermelonColor() -> UIColor {
-        return hsb(356, 53, 94)
+    static var flatWatermelonColor: UIColor {
+        hsb(356, 53, 94)
     }
     
-    class func flatWhiteColor() -> UIColor {
-        return hsb(192, 2, 95)
+    static var flatWhiteColor: UIColor {
+        hsb(192, 2, 95)
     }
     
-    class func flatYellowColor() -> UIColor {
-        return hsb(48, 99, 100)
+    static var flatYellowColor: UIColor {
+        hsb(48, 99, 100)
     }
     
-    class func flatBlackColorDark() -> UIColor {
-        return hsb(0, 0, 15)
+    static var flatBlackColorDark: UIColor {
+        hsb(0, 0, 15)
     }
     
-    class func flatBlueColorDark() -> UIColor {
-        return hsb(224, 56, 51)
+    static var flatBlueColorDark: UIColor {
+        hsb(224, 56, 51)
     }
     
-    class func flatBrownColorDark() -> UIColor {
-        return hsb(25, 45, 31)
+    static var flatBrownColorDark: UIColor {
+        hsb(25, 45, 31)
     }
     
-    class func flatCoffeeColorDark() -> UIColor {
-        return hsb(25, 34, 56)
+    static var flatCoffeeColorDark: UIColor {
+        hsb(25, 34, 56)
     }
     
-    class func flatForestGreenColorDark() -> UIColor {
-        return hsb(135, 44, 31)
+    static var flatForestGreenColorDark: UIColor {
+        hsb(135, 44, 31)
     }
     
-    class func flatGrayColorDark() -> UIColor {
-        return hsb(184, 10, 55)
+    static var flatGrayColorDark: UIColor {
+        hsb(184, 10, 55)
     }
     
-    class func flatGreenColorDark() -> UIColor {
-        return hsb(145, 78, 68)
+    static var flatGreenColorDark: UIColor {
+        hsb(145, 78, 68)
     }
     
-    class func flatLimeColorDark() -> UIColor {
-        return hsb(74, 81, 69)
+    static var flatLimeColorDark: UIColor {
+        hsb(74, 81, 69)
     }
     
-    class func flatMagentaColorDark() -> UIColor {
-        return hsb(282, 61, 68)
+    static var flatMagentaColorDark: UIColor {
+        hsb(282, 61, 68)
     }
     
-    class func flatMaroonColorDark() -> UIColor {
-        return hsb(4, 68, 40)
+    static var flatMaroonColorDark: UIColor {
+        hsb(4, 68, 40)
     }
     
-    class func flatMintColorDark() -> UIColor {
-        return hsb(168, 86, 63)
+    static var flatMintColorDark: UIColor {
+        hsb(168, 86, 63)
     }
     
-    class func flatNavyBlueColorDark() -> UIColor {
-        return hsb(210, 45, 31)
+    static var flatNavyBlueColorDark: UIColor {
+        hsb(210, 45, 31)
     }
     
-    class func flatOrangeColorDark() -> UIColor {
-        return hsb(24, 100, 83)
+    static var flatOrangeColorDark: UIColor {
+        hsb(24, 100, 83)
     }
     
-    class func flatPinkColorDark() -> UIColor {
-        return hsb(327, 57, 83)
+    static var flatPinkColorDark: UIColor {
+        hsb(327, 57, 83)
     }
     
-    class func flatPlumColorDark() -> UIColor {
-        return hsb(300, 46, 31)
+    static var flatPlumColorDark: UIColor {
+        hsb(300, 46, 31)
     }
     
-    class func flatPowderBlueColorDark() -> UIColor {
-        return hsb(222, 28, 84)
+    static var flatPowderBlueColorDark: UIColor {
+        hsb(222, 28, 84)
     }
     
-    class func flatPurpleColorDark() -> UIColor {
-        return hsb(253, 56, 64)
+    static var flatPurpleColorDark: UIColor {
+        hsb(253, 56, 64)
     }
     
-    class func flatRedColorDark() -> UIColor {
-        return hsb(6, 78, 75)
+    static var flatRedColorDark: UIColor {
+        hsb(6, 78, 75)
     }
     
-    class func flatSandColorDark() -> UIColor {
-        return hsb(42, 30, 84)
+    static var flatSandColorDark: UIColor {
+        hsb(42, 30, 84)
     }
     
-    class func flatSkyBlueColorDark() -> UIColor {
-        return hsb(204, 78, 73)
+    static var flatSkyBlueColorDark: UIColor {
+        hsb(204, 78, 73)
     }
     
-    class func flatTealColorDark() -> UIColor {
-        return hsb(196, 54, 45)
+    static var flatTealColorDark: UIColor {
+        hsb(196, 54, 45)
     }
     
-    class func flatWatermelonColorDark() -> UIColor {
-        return hsb(358, 61, 85)
+    static var flatWatermelonColorDark: UIColor {
+        hsb(358, 61, 85)
     }
     
-    class func flatWhiteColorDark() -> UIColor {
-        return hsb(204, 5, 78)
+    static var flatWhiteColorDark: UIColor {
+        hsb(204, 5, 78)
     }
     
-    class func flatYellowColorDark() -> UIColor {
-        return hsb(40, 100, 100)
+    static var flatYellowColorDark: UIColor {
+        hsb(40, 100, 100)
     }
     
 }
